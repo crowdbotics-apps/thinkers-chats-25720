4d6159e674c9126f0c6fa2f2dc0158a21ccb69d1
Crowdbotics <team@crowdbotics.com>
2021-04-15 14:14:58 +0000

    Update models

--- thinkers_chats_25720/wsgi.py
+++ thinkers_chats_25720/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thinkers_chats_25720.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkers_chats_25720.settings")
 
 application = get_wsgi_application()
